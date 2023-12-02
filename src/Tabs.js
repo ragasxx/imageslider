03524765e3cf1e51bcf71547d309fc71e352cb1a
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-12-02 11:10:33 +0530

    tabs

---
+++ src/Tabs.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+const Tabs = () => {
+  const tabs = [
+    {
+      id: 1,
+      tabTitle: "Tab1",
+      title: "Title 1",
+      content: "a",
+    },
+    {
+      id: 2,
+      tabTitle: "Tab2",
+      title: "Title 2",
+      content: "b",
+    },
+    {
+      id: 3,
+      tabTitle: "Tab3",
+      title: "Title 3",
+      content: "c",
+    },
+  ];
+
+  const [activeTab, setActiveTab] = useState();
+
+  const handleClick = (id) => {
+    setActiveTab(id);
+  };
+
+  console.log(activeTab);
+
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-[700px] h-[500px] border border-black">
+        <div className="flex justify-between h-15">
+          {tabs.map((tab, i) => {
+            return (
+              <button
+                key={tab.id}
+                id={tab.id}
+                className={
+                  "p-2 m-2 w-full font-bold cursor-pointer bg-gradient-to-r from-slate-400 to-slate-700 " +
+                  (activeTab === tab.id
+                    ? "border border-b-orange-700 border-b-4"
+                    : "")
+                }
+                onClick={() => handleClick(tab.id)}
+              >
+                {tab.tabTitle}
+              </button>
+            );
+          })}
+        </div>
+        <div>
+          {tabs.map((tab) => {
+            return <div>{activeTab === tab.id && <p>{tab.title}</p>}</div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
